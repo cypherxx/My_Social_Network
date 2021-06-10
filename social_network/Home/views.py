a60cf11eea4425f2da5a50dd33016b57a7d9226b
cypherxx <vaishnavijahav@gmail.com>
2021-06-10 13:54:20 +0530

    profile model added

--- social_network/Home/views.py
+++ social_network/Home/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse, JsonResponse
 from django.contrib.auth import authenticate, login, logout
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm,UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from .models import Profile
 
 def dashboard(request):
    
@@ -17,17 +18,21 @@ def index(request):
 def signup(request):
     if request.method=='GET':
         return render(request,'Registration/signup.html')
-    else:
+    elif request.method=='POST':
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
        
             new_user = user_form.save(commit=False)
         
-            new_user.set_password(
-            user_form.cleaned_data['password'])
+            new_user.set_password(user_form.cleaned_data['password'])
         
             new_user.save()
-        return render(request,'account/register_done.html',{'new_user': new_user})
+            Profile.objects.create(user=new_user)
+            return render(request,'Registration/register_done.html',{'new_user': new_user})
+        else:
+            user_form = UserRegistrationForm()
+            return render(request,'Registration/signup.html',{'user_form': user_form})
+
 
 def logout_user(request):
     logout(request)
@@ -48,3 +53,24 @@ def user_login(request):
     else:
         form=LoginForm()
     return render(request, 'Registration/login.html', {'form':form})
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user,
+        data=request.POST)
+        profile_form = ProfileEditForm(
+        instance=request.user.profile,
+        data=request.POST,
+        files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(
+        instance=request.user.profile)
+    return render(request,'Registration/edit.html',{'user_form': user_form,'profile_form': profile_form})
